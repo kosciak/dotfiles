19214c09973f1d71a154f764cf194b4d89600a3a
Wojciech Pietrzok <kosciak@kosciak.net>
2022-12-13 17:59:52 +0100

    etc/profile.d/color_prompt.sh - default colors .bashrc.d/prompt.sh - simplified prompt - no user@host, only venv, path, git info

--- etc/profile.d/color_prompt.sh
+++ etc/profile.d/color_prompt.sh
@@ -81,8 +81,8 @@ if [[ ! -z $BASH ]]; then
     if [[ $EUID -eq 0 ]]; then
 		# Logged as root
 		PS1_PRE="${RESET}"
-		# PS1_PRE+="${YELLOW}[${RESET}"
-		PS1_PRE+="${BOLD}${RED}\u${RESET}"
+		PS1_PRE+="${YELLOW}[${RESET}"
+		PS1_PRE+="${RED}\u${RESET}"
 		PS1_PRE+="@"
 		PS1_PRE+="${RED}\h${RESET}"
 		PS1_PRE+=" "
@@ -90,72 +90,26 @@ if [[ ! -z $BASH ]]; then
 		PS1_PRE+="${RESET}"
 
 		PS1_POST="${RESET}"
-		# PS1_POST+="${YELLOW}]${RESET}"
+		PS1_POST+="${YELLOW}]${RESET}"
 		PS1_POST+=" \$ "
 		PS1_POST+="${RESET}"
     else
 		# Normal user
 		PS1_PRE="${RESET}"
-		# PS1_PRE+="${CYAN}[${RESET}"
-		# PS1_PRE+="${BLUE}\u${RESET}"
-		# PS1_PRE+="@"
-		# PS1_PRE+="${BLUE}\h${RESET}"
+		PS1_PRE+="${CYAN}[${RESET}"
+		PS1_PRE+="${BLUE}\u${RESET}"
+		PS1_PRE+="@"
+		PS1_PRE+="${BLUE}\h${RESET}"
 		PS1_PRE+=" "
-		PS1_PRE+="${BOLD}${BLUE}\w${RESET}"
+		PS1_PRE+="${GREEN}\w${RESET}"
 		PS1_PRE+="${RESET}"
 
 		PS1_POST="${RESET}"
-		# PS1_POST+="${CYAN}]${RESET}"
+		PS1_POST+="${CYAN}]${RESET}"
 		PS1_POST+=" \$ "
 		PS1_POST+="${RESET}"
     fi
 
 	export PS1="${PS1_PRE}${PS1_POST}"
 fi;
-
-
-__venv_ps1() {
-	printf "${MAGENTA} %s${RESET}" "${VIRTUAL_ENV##*/}"
-}
-
-
-## Set PROMPT_COMMAND
-#  Executed before PS*
-prompt_command() {
-
-	# Change title of terminals
-	case ${TERM} in
-		xterm*|rxvt*|Eterm*|aterm|kterm|gnome*|alacritty|st|konsole*)
-			echo -ne "\033]0;${USER}@${HOSTNAME%%.*}:${PWD/#$HOME/\~}\007"
-			;;
-		screen*)
-			echo -ne "\033_${USER}@${HOSTNAME%%.*}:${PWD/#$HOME/\~}\033\\"
-			;;
-	esac;
-
-	# Support OSC7 for VTE-based terminals (e.g. tilix) if existing
-	# See: /etc/profile.d/vte.sh
-	# NOTE: In gnome-terminal allows opening new tabs in current working dir
-	hash __vte_osc7 &> /dev/null && __vte_osc7
-
-	# Set PS* prompts
-	PS1="${PS1_PRE}${PS1_POST}"
-
-	# Add git prompt if available
-	# NOTE: git-prompt coloring works only when called from PROMPT_COMMAND
-	# NOTE: It will replace PS1 with new value!
-	if type __git_ps1 &> /dev/null; then
-		# __git_ps1 "${PS1_PRE}" "${PS1_POST}" " %s"
-		__git_ps1 "${PS1_PRE}" "${PS1_POST}" " @%s"
-	fi;
-
-	# Add Virtual ENV name to prompt
-	if [[ -n "$VIRTUAL_ENV" ]]; then
-		PS1="$(__venv_ps1)${PS1}"
-	fi;
-
-	export PS1
-
-}
-export PROMPT_COMMAND="prompt_command"
 
