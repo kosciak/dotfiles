ae555ef4d98b8174058281ba1bb577b416f4e9e0
Wojciech Pietrzok <kosciak@kosciak.net>
2023-01-18 18:35:28 +0100

    .bashrc.d/man_pager.sh - option to use vim as man pager

--- .bashrc.d/man_pager.sh
+++ .bashrc.d/man_pager.sh
@@ -6,3 +6,7 @@
 # See: https://stackoverflow.com/questions/1049350/how-to-make-less-indicate-location-in-percentage/19871578#19871578
 export MANPAGER='less -s -M +Gg'
 
+# Use: vim
+# NOTE: ftplugin/man.vim must be enabled
+# export MANPAGER="vim +MANPAGER --not-a-term -"
+
