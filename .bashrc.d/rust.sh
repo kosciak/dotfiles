c04677de33f047ad7131fe5f35eea05a1f53bd68
Wojciech Pietrzok <kosciak@kosciak.net>
2023-01-27 13:25:37 +0100

    .bashrc.d/rust.sh - check if path exists before sourcing

--- .bashrc.d/rust.sh
+++ .bashrc.d/rust.sh
@@ -1,4 +1,8 @@
 
 # Rust evironment and toolchain management with rustup
-. "$HOME/.cargo/env"
+# See: https://rustup.rs/
+# Install: curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+if [ -f  "$HOME/.cargo/env" ]; then
+    . "$HOME/.cargo/env"
+fi
 
