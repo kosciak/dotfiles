fff6ba3f99c2a3d81b95c42587b32cef47f28661
Wojciech Pietrzok <kosciak@kosciak.net>
2023-02-09 20:33:09 +0100

    Bash     - export XDG_*_HOME directories     - simple fzf setup

--- .bashrc.d/exports.sh
+++ .bashrc.d/exports.sh
@@ -1,4 +1,9 @@
 
+## Locale
+# Force english
+# export LC_ALL=C
+
+
 ## History
 # Don't put duplicate lines or lines starting with space in the history.
 # Remove previous duplicates
@@ -6,7 +11,21 @@ export HISTCONTROL=ignorespace:ignoredups:erasedups
 export HISTSIZE=10000
 
 
+## Setup XDG_* directories
+#  See: https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
 if [ -z "$XDG_CONFIG_HOME" ] ; then
     export XDG_CONFIG_HOME="$HOME/.config"
+fi
+
+if [ -z "$XDG_CACHE_HOME" ] ; then
+    export XDG_CACHE_HOME="$HOME/.cache"
+fi
+
+if [ -z "$XDG_DATA_HOME" ] ; then
+    export XDG_DATA_HOME="$HOME/.local/share"
+fi
+
+if [ -z "$XDG_STATE_HOME" ] ; then
+    export XDG_STATE_HOME="$HOME/.local/state"
 fi
 
---
+++ .bashrc.d/fzf.sh
@@ -0,0 +1,5 @@
+## FZF fuzzy finder
+#
+# Use fd as default search; show hidden files, respect .gitignore
+export FZF_DEFAULT_COMMAND='fd --type f --strip-cwd-prefix --hidden --follow --exclude .git'
+
