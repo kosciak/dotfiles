6eb61acc062a0945cea38cd2a84d061b1b438015
Wojciech Pietrzok <kosciak@kosciak.net>
2026-01-12 14:26:16 +0100

    .bashrc.d/kitty-kittens.sh  - aliases for icat and ssh kittens when in kitty terminal

---
+++ .bashrc.d/kitty-kittens.sh
@@ -0,0 +1,10 @@
+## Kitty terminal extensions
+#
+# See: https://sw.kovidgoyal.net/kitty/kittens_intro/
+#
+
+if [ "$TERM" = "xterm-kitty" ]; then
+    alias icat='kitten icat'
+    alias ssh='kitten ssh'
+fi
+
