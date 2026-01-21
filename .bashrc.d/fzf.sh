705dd4b22c786361da459b2fe417138f21641502
Wojciech Pietrzok <kosciak@kosciak.net>
2026-01-21 20:25:31 +0100

    .bashrc.d/fzf.sh  - ctrl-y for toggle-previe

--- .bashrc.d/fzf.sh
+++ .bashrc.d/fzf.sh
@@ -12,7 +12,8 @@ export FZF_DEFAULT_OPTS+=' --prompt=\>\>\>\  --marker=+'
 export FZF_DEFAULT_OPTS+=" --bind='ctrl-w:backward-kill-word'"
 export FZF_DEFAULT_OPTS+=" --bind='ctrl-a:toggle-all'"
 export FZF_DEFAULT_OPTS+=" --bind='ctrl-o:toggle-sort'"
-export FZF_DEFAULT_OPTS+=" --bind='ctrl-w:toggle-preview'"
+export FZF_DEFAULT_OPTS+=" --bind='ctrl-/:toggle-preview'"
+export FZF_DEFAULT_OPTS+=" --bind='ctrl-y:toggle-preview'"
 export FZF_DEFAULT_OPTS+=' --color=light,hl:6,fg+:regular,bg+:254,hl+:6,'
 export FZF_DEFAULT_OPTS+='prompt:regular:130,query:regular,info:4,separator:7,'
 export FZF_DEFAULT_OPTS+='pointer:regular:4,marker:bold:1,'
